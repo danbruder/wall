c72345b3737c14de6e64b2ecce652f23ac5c80ea
Dan Bruder <danb@structionsite.com>
2020-12-23 11:15:16 -0500

    upload route

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 // #![deny(warnings)]
+use bytes::{BufMut, BytesMut};
+
+use futures::TryStreamExt;
+use futures::{FutureExt, StreamExt};
 use std::collections::HashMap;
+use std::convert::Infallible;
 use std::env;
 use std::sync::{
     atomic::{AtomicUsize, Ordering},
     Arc,
 };
-
-use futures::{FutureExt, StreamExt};
 use tokio::sync::{mpsc, RwLock};
+use uuid::Uuid;
 use warp::ws::{Message, WebSocket};
-use warp::Filter;
+use warp::{
+    http::StatusCode,
+    multipart::{FormData, Part},
+    Filter, Rejection, Reply,
+};
 
 /// Our global unique user id counter.
 static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
@@ -46,10 +54,17 @@ async fn main() {
             ws.on_upgrade(move |socket| user_connected(socket, users))
         });
 
+    let upload_route = warp::path("upload")
+        .and(warp::post())
+        .and(warp::multipart::form().max_length(5_000_000))
+        .and_then(upload);
     let assets = warp::path("public").and(warp::fs::dir("assets/public/dist"));
     let index = warp::fs::file("assets/public/dist/index.html");
-
-    let routes = chat.or(assets).or(index);
+    let routes = upload_route
+        .or(chat)
+        .or(assets)
+        .or(index)
+        .recover(handle_rejection);
 
     warp::serve(routes).run(([0, 0, 0, 0], port)).await;
 }
@@ -63,8 +78,6 @@ async fn user_connected(ws: WebSocket, users: Users) {
     // Split the socket into a sender and receive of messages.
     let (user_ws_tx, mut user_ws_rx) = ws.split();
 
-    // Use an unbounded channel to handle buffering and flushing of messages
-    // to the websocket...
     let (tx, rx) = mpsc::unbounded_channel();
     tokio::task::spawn(rx.forward(user_ws_tx).map(|result| {
         if let Err(e) = result {
@@ -72,17 +85,9 @@ async fn user_connected(ws: WebSocket, users: Users) {
         }
     }));
 
-    // Save the sender in our list of connected users.
     users.write().await.insert(my_id, tx);
-
-    // Return a `Future` that is basically a state machine managing
-    // this specific user's connection.
-
-    // Make an extra clone to give to our disconnection handler...
     let users2 = users.clone();
 
-    // Every time the user sends a message, broadcast it to
-    // all other users...
     while let Some(result) = user_ws_rx.next().await {
         let msg = match result {
             Ok(msg) => msg,
@@ -94,13 +99,10 @@ async fn user_connected(ws: WebSocket, users: Users) {
         user_message(my_id, msg, &users).await;
     }
 
-    // user_ws_rx stream will keep processing as long as the user stays
-    // connected. Once they disconnect, then...
     user_disconnected(my_id, &users2).await;
 }
 
 async fn user_message(my_id: usize, msg: Message, users: &Users) {
-    // Skip any non-Text messages...
     let msg = if let Ok(s) = msg.to_str() {
         s
     } else {
@@ -111,17 +113,75 @@ async fn user_message(my_id: usize, msg: Message, users: &Users) {
 
     // New message from this user, send it to everyone else (except same uid)...
     for (_uid, tx) in users.read().await.iter() {
-        if let Err(_disconnected) = tx.send(Ok(Message::text(new_msg.clone()))) {
-            // The tx is disconnected, our `user_disconnected` code
-            // should be happening in another task, nothing more to
-            // do here.
-        }
+        if let Err(_disconnected) = tx.send(Ok(Message::text(new_msg.clone()))) {}
     }
 }
 
 async fn user_disconnected(my_id: usize, users: &Users) {
-    eprintln!("good bye user: {}", my_id);
-
-    // Stream closed up, so remove from the user list
     users.write().await.remove(&my_id);
+}
+
+async fn upload(form: FormData) -> Result<impl Reply, Rejection> {
+    let parts: Vec<Part> = form.try_collect().await.map_err(|e| {
+        eprintln!("form error: {}", e);
+        warp::reject::reject()
+    })?;
+
+    for p in parts {
+        if p.name() == "file" {
+            let content_type = p.content_type();
+            let file_ending = match content_type {
+                Some(file_type) => match file_type {
+                    "image/png" => "png",
+                    "image/jpeg" => "jpeg",
+                    "image/jpg" => "jpg",
+                    v => {
+                        eprintln!("invalid file type found: {}", v);
+                        return Err(warp::reject::reject());
+                    }
+                },
+                None => {
+                    eprintln!("file type could not be determined");
+                    return Err(warp::reject::reject());
+                }
+            };
+
+            let value = p
+                .stream()
+                .try_fold(BytesMut::new(), |mut vec, data| {
+                    vec.put(data);
+                    async move { Ok(vec) }
+                })
+                .await
+                .map_err(|e| {
+                    eprintln!("reading file error: {}", e);
+                    warp::reject::reject()
+                })?;
+
+            let file_name = format!("./files/{}.{}", Uuid::new_v4().to_string(), file_ending);
+            tokio::fs::write(&file_name, value).await.map_err(|e| {
+                eprint!("error writing file: {}", e);
+                warp::reject::reject()
+            })?;
+            println!("created file: {}", file_name);
+        }
+    }
+
+    Ok("success")
+}
+
+async fn handle_rejection(err: Rejection) -> std::result::Result<impl Reply, Infallible> {
+    let (code, message) = if err.is_not_found() {
+        (StatusCode::NOT_FOUND, "Not Found".to_string())
+    } else if err.find::<warp::reject::PayloadTooLarge>().is_some() {
+        (StatusCode::BAD_REQUEST, "Payload too large".to_string())
+    } else {
+        eprintln!("unhandled error: {:?}", err);
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Internal Server Error".to_string(),
+        )
+    };
+
+    Ok(warp::reply::with_status(message, code))
 }
